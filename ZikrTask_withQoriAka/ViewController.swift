d2c46240a525a94b416844975593beca66393df1
faxriddin0516 <faxaoff@gmail.com>
2024-05-21 17:35:54 +0500

    Initial Commit

---
+++ ZikrTask_withQoriAka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZikrTask_withQoriAka
+//
+//  Created by Faxriddin Mo'ydinxonov on 21/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
